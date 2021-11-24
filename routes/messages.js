267d1fbc4ce666ca6c07df2df6d82e9309e74d3b
robwilliams-it <robwilliams.it@gmail.com>
2021-11-24 12:30:09 -0600

    updated the start script

--- routes/messages.js
+++ routes/messages.js
@@ -5,4 +5,8 @@ router.get('/', function (req, res) {
   res.render('messages/index', { adjective: 'here' })
 })
 
+router.get('/a', function (req, res) {
+  res.render('components/newMessageForm', { adjective: 'here' })
+})
+
 module.exports = router
